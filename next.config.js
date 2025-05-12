6621d4d495dfab1058ab85f241d02dfc7460560b
work-rjkashyap <work.rjkashyap05@gmail.com>
2025-05-12 23:41:59 +0530

    fix: update GitHub Actions workflow for Next.js deployment and enhance configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,14 +1,18 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.NODE_ENV === 'production';
+const repoName = 'work-rjkashyap.github.io'; // 🔁 Replace this with your actual GitHub repo name
+
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	trailingSlash: true,
+	basePath: isGithubPages ? `/${repoName}` : '',
+	assetPrefix: isGithubPages ? `/${repoName}/` : '',
 	experimental: {
 		images: {
 			unoptimized: true,
 		},
 	},
-	basePath: '',
-	assetPrefix: '',
 };
 
 module.exports = nextConfig;
